6325059f7a84edd255e443a65c0006d87842bae5
Marc Marcos Laptop <marcmarcosmadruga@gmail.com>
2021-08-01 18:27:09 +0200

    First commit
    Simple server and client.

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('192.168.1.139', 420))
+
+while True:
+    string_data = input('>>>')
+    if string_data == 'exit':
+        break
+
+    else:
+        s.send(bytes(string_data, encoding='utf8'))
