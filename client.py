8d9a609be63d0ec25c9052278e5e981c41ee8f91
Marc Marcos <marcmarcosmadruga@gmail.com>
2021-08-30 09:56:16 +0200

    Reestructurate the file system

--- client.py
+++ client.py
@@ -1,39 +1,12 @@
 import socket
-import threading
-import pyfirmata
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-sock.bind(('0.0.0.0', 420))
-sock.listen(1)
-connections = []
-
-pin = (24)
-puerto = "\\.\ACM0"
-tarjeta = pyfirmata.ArduinoMega(puerto)
-
-
-def handler(c, a):
-    global connections
-    while True:
-        data = c.recv(1024)
-        decodedData = data.decode()
-
-        if decodedData == 'encender':
-            tarjeta.digital[pin].write(1)
-
-        elif decodedData == 'apagar':
-            tarjeta.digital[pin].write(0)
-
-        if not data:
-            connections.remove(c)
-            c.close()
-            break
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('192.168.0.226', 420))
 
 while True:
-    c, a = sock.accept()
-    cThread = threading.Thread(target=handler, args=(c, a))
-    cThread.daemon = True
-    cThread.start()
-    connections.append(c)
-    print(connections)
+    string_data = input('>>>')
+    if string_data == 'exit':
+        break
+
+    else:
+        s.send(bytes(string_data, encoding='utf8'))
---
+++ serverArduino.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+import pyfirmata
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.bind(('0.0.0.0', 420))
+sock.listen(1)
+connections = []
+
+pin = (24)
+puerto = "\\.\ACM0"
+tarjeta = pyfirmata.ArduinoMega(puerto)
+
+
+def handler(c, a):
+    global connections
+    while True:
+        data = c.recv(1024)
+        decodedData = data.decode()
+
+        if decodedData == 'encender':
+            tarjeta.digital[pin].write(1)
+
+        elif decodedData == 'apagar':
+            tarjeta.digital[pin].write(0)
+
+        if not data:
+            connections.remove(c)
+            c.close()
+            break
+
+while True:
+    c, a = sock.accept()
+    cThread = threading.Thread(target=handler, args=(c, a))
+    cThread.daemon = True
+    cThread.start()
+    connections.append(c)
+    print(connections)
