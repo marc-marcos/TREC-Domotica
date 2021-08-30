6d9bfecda0da7902b3d52188315eb66c108d7a02
Marc Marcos <marcmarcosmadruga@gmail.com>
2021-08-30 13:01:05 +0200

    Update

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import pyfirmata
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -7,18 +8,28 @@ sock.bind(('0.0.0.0', 420))
 sock.listen(1)
 connections = []
 
+pin = (24)
+puerto = "\\.\ACM0" #/dev/tty/ACM0
+tarjeta = pyfirmata.ArduinoMega(puerto)
+
 
 def handler(c, a):
     global connections
+
     while True:
         data = c.recv(1024)
-        print(data.decode())
+        decodedData = data.decode()
+
+        if decodedData == 'turnOn':
+            tarjeta.digital[pin].write(1)
+
+        if decodedData == 'turnOff':
+            tarjeta.digital[pin].write(0)
 
         if not data:
             connections.remove(c)
             c.close()
             break
-
 
 while True:
     c, a = sock.accept()
@@ -26,4 +37,4 @@ while True:
     cThread.daemon = True
     cThread.start()
     connections.append(c)
-    print(connections)
+    #print(connections)
