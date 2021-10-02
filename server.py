9dba7cf8e302385c8fec62262abe05fb3ec110fb
Marc Marcos <marcmarcosmadruga@gmail.com>
2021-10-02 19:06:05 +0200

    missed commit

--- server.py
+++ server.py
@@ -13,9 +13,19 @@ connections = []
 pin1 = (2)
 pin2 = (3)
 pin3 = (4)
+pinsList = (2, 3, 4)
+
+pinAuto = (5)
 puerto = "/dev/ttyACM0"
 tarjeta = pyfirmata.ArduinoMega(puerto)
 
+def turnAllOff(pins):
+    for i in pins:
+        tarjeta.digital[i].write(0)
+
+def turnAllOn(pins):
+    for i in pins:
+        tarjeta.digital[i].write(1)
 
 def handler(c, a):
     global connections
@@ -23,6 +33,9 @@ def handler(c, a):
     while True:
         data = c.recv(1024)
         decodedData = data.decode()
+        
+        if decodedData == 'turnAllOff':
+            turnAllOff(pinsList)
 
         if decodedData == 'turnOn1':
             tarjeta.digital[pin1].write(1)
@@ -42,10 +55,20 @@ def handler(c, a):
         if decodedData == 'turnOff3':
             tarjeta.digital[pin3].write(0)
 
+        if decodedData == 'turnOnAuto':
+            tarjeta.digital[pinAuto].write(1)
+            turnAllOff(pinsList)
+            
+        
+        if decodedData == 'turnOffAuto':
+            tarjeta.digital[pinAuto].write(0)
+            turnAllOn(pinsList)
+
         if not data:
             connections.remove(c)
             c.close()
             break
+
 
         print(decodedData)
 
